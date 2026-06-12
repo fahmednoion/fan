fa5e79352e9346c76437671471d21bc5d210db22
fahmednoion <fahmednoion@gmail.com>
2026-06-13 01:22:46 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -397,9 +397,3 @@ def home():
 
 def run_bot():
     main()
-
-if __name__ == "__main__":
-    threading.Thread(target=run_bot).start()
-
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
